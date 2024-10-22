fc7c46005b6decd163639b3d4ff77799521eef70
GabrielRiveraOfficial <gabriverlay@gmail.com>
2024-10-22 18:46:12 -0500

    fix(shared): updated generic repository

--- TakeMyCarPlatform.API/Shared/Domain/Repositories/IBaseRepository.cs
+++ TakeMyCarPlatform.API/Shared/Domain/Repositories/IBaseRepository.cs
@@ -3,7 +3,7 @@
     public interface IBaseRepository<TEntity>
     {
         Task AddAsync(TEntity entity);
-        Task<TEntity?> FindByAsync(int id);
+        Task<TEntity?> FindByIdAsync(int id);
         void Update(TEntity entity);
         void Remove(TEntity entity);
         Task<IEnumerable<TEntity>> ListAsync();
